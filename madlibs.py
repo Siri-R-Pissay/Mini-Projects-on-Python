55db60f92b66f8a5d20e5d3cc14a5c2ed9b471f8
Siri R Pissay <144196730+Siri-R-Pissay@users.noreply.github.com>
2024-02-25 19:40:01 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+story = "Once upon a time, there was a {} {} who {} {}."
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+print(story.format(adjective, noun, verb, adverb))
+
